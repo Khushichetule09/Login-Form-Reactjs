89806e0f77893a3a05cc3557ccab8a53d16bc8a9
khushichetule09 <khushichetule2@gmail.com>
2025-07-26 16:21:19 +0530

    Login-Form

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+const App = () => {
+  return (
+    <div className="login-container">
+      <h2 className="form-title">Log in with</h2>
+      <div className="social-login">
+        <button className="social-button">
+          <img src="google.svg" alt="Google" className="social-icon" />
+          Google
+          </button>
+          <button className="social-button">
+            <img src="apple.svg" alt="Apple" className="social-icon" />
+            Apple
+            </button>
+         </div>
+
+         <p className="separate"><span>or</span></p>
+
+         <form action="#" className="loin-form">
+          <div className="input-wrapper">
+            <input type="email" placeholder="Email address"
+            className="input-field" required />
+            <i className="material-symbols-rounded">mail</i>
+          </div>
+
+          <div className="input-wrapper">
+            <input type="password" placeholder="Password"
+            className="input-field" required />
+            <i className="material-symbols-rounded">lock</i>
+          </div>
+          <a href="#" className="forgot-pass-link">Forgot Password?</a>
+
+          <button className="login-button">Log In</button>
+         </form>
+
+         <p className="signup-text">Don&apos;t have a account? <a href="#">Signup now</a></p>
+    </div>
+  )
+}
+
+export default App;
